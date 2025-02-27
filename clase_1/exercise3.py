8f5a029ca004991cebba32dfcab7fbdf2357bb01
carek2025 <1560977@senati.pe>
2025-02-27 14:58:33 -0500

    tercera semana de seminario

--- clase_1/exercise3.py
+++ clase_1/exercise3.py
@@ -6,42 +6,17 @@ def es_primo(n):
             return False
     return True
 
-def lista_de_primos(inicio,final):
+def lista_de_primos(inicio,final=1000):
     primos=list()
     for i in range(inicio,final):
         if es_primo(i):
             primos.append(i)
+            if len(primos)==20:
+                break
     print(primos)
 
-lista_de_primos(1,20)
+lista_de_primos(1)
 
 
 #algoritmos turing y algoritmo burbuja y redes neurales y arboles binarios y mergesort
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def es_primo(num):
-    if num <=1:
-        return False
-    for i in range(2,num//2):
-        if
