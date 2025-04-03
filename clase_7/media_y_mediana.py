a87161d991d0acf47ab60900ec2f91a78961296e
carek2025 <1560977@senati.pe>
2025-04-03 18:02:43 -0500

    Seminario completado, Realizamos ejerciciosde analisis de datos como calcular media, mediana, desviacion estandar y varianza

--- clase_7/media_y_mediana.py
+++ clase_7/media_y_mediana.py
@@ -5,8 +5,6 @@ datos = [25, 30, 35, 40, 45, 50]
 
 media = np.mean(datos)
 mediana = np.median(datos)
-varianza = np.var(datos)  # Varianza
-desviacion_estandar = np.std(datos)  # Desviación estándar
 print(f"Media: {media}")
 print(f"Mediana: {mediana}")
 
@@ -14,10 +12,7 @@ plt.bar(range(len(datos)), datos, color='skyblue', label='Datos')
 
 plt.axhline(media, color='red', linestyle='dashed', linewidth=5, label=f'Media: {media}')
 plt.axhline(mediana, color='blue', linestyle='dashed', linewidth=2, label=f'Mediana: {mediana}')
-plt.axhline(media + desviacion_estandar, color='green', label=f'Media + 1 Desviación Estándar: {media + desviacion_estandar:.2f}', linestyle='dotted', linewidth=2)
-plt.axhline(media - desviacion_estandar, color='green', label=f'Media - 1 Desviación Estándar: {media - desviacion_estandar:.2f}', linestyle='dotted', linewidth=2)
-plt.axhline(varianza, color='yellow', label=f'varianza: {varianza:.2f}', linestyle='dotted', linewidth=2)
-plt.axhline(desviacion_estandar, color='black', label=f'Desviación Estándar: {desviacion_estandar:.2f}', linestyle='dotted', linewidth=2)
+
 
 plt.xlabel('Índices')
 plt.ylabel('Valores')
